1e5b3bd5d2be3830b340355004c78e4b7fa5b114
pawlo4-94 <pawlo4-94@o2.pl>
2021-06-03 14:38:00 +0200

    change task content

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Container from "./Container";
 
 const tasks = [
   { id: 1, content: "Przejść na Reacta", done: false },
-  { id: 2, content: "Zjeść Obiad", done: true },
+  { id: 2, content: "Zjeść Kolację", done: true },
 ];
 
 const hideDoneTasks = false;
