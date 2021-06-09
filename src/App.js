f6937fc4be588a8c7a45af3317c1e7c64614ca38
pawlo4-94 <pawlo4-94@o2.pl>
2021-06-09 17:20:37 +0200

    toggle task done interaction

--- src/App.js
+++ src/App.js
@@ -22,6 +22,15 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+      return task;
+    }));
+  };
+
   return (
     <Container>
       <Header title="Lista Zadań" />
@@ -34,6 +43,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
           />
         }
         extraHeaderContent={
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone, removeTask }) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li
                 key={task.id}
                 className={`tasks__item ${hideDone && task.done ? "tasks__item--hidden" : ""}`}
             >
-                <button className="tasks__button tasks__button--done">
+                <button
+                    className="tasks__button tasks__button--done"
+                    onClick={() => toggleTaskDone(task.id)}
+                >
                     {task.done ? "✔" : ""}
                 </button>
                 <span className={`tasks__span ${task.done ? "tasks__span--done" : ""}`}>
