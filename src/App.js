bc638dac7c61ede1582fa1e7dff5ba053e7896bc
pawlo4-94 <pawlo4-94@o2.pl>
2021-06-09 17:45:59 +0200

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -38,12 +38,25 @@ function App() {
     })));
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks =>
+      [...tasks,
+      {
+        content,
+        done: false,
+        id: !tasks.length ? 1 : tasks[tasks.length - 1].id + 1,
+      },
+      ]);
+  };
+
   return (
     <Container>
       <Header title="Lista Zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form />} />
+        body={<Form
+          addNewTask={addNewTask}
+        />} />
       <Section title="Lista zadań"
         body={
           <Tasks
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-    <form className="form">
-        <input className="form__input" placeholder="Co jest do zrobienia?" required />
-        <button className="form__button">Dodaj zadanie</button>
-    </form>
-);
+const Form = ({ addNewTask }) => {
+    const [newTaskContent, setNewTaskContent] = useState("");
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        if (!newTaskContent.trim()) {
+            setNewTaskContent("");
+            return;
+        }
+        addNewTask(newTaskContent.trim());
+        setNewTaskContent("");
+    }
+
+    return (
+        <form
+            className="form"
+            onSubmit={onFormSubmit}
+        >
+            <input
+                className="form__input"
+                placeholder="Co jest do zrobienia?"
+                value={newTaskContent}
+                onChange={({ target }) => setNewTaskContent(target.value)}
+                required
+            />
+            <button className="form__button">Dodaj zadanie</button>
+        </form>
+    )
+};
 
 export default Form;
